3cc08234521aecdfe3976e81a19884ab7955c941
331qjs <331qjs@gmail.com>
2021-08-24 16:05:06 +0900

    4주차 실습

---
+++ sp4/types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+	printf("The size fo some fundamental types is computed.\n\n");
+	printf("char:%3ld byte \n", sizeof(char));
+	printf("short:%3ld bytes \n", sizeof(short));
+	printf("int:%3ld bytes \n", sizeof(int));
+	printf("long:%3ld bytes\n", sizeof(long));
+	printf("unsigned:%3ld bytes\n", sizeof(unsigned));
+	printf("float:%3ld bytes\n", sizeof(float));
+	printf("double:%3ld bytes\n", sizeof(double));
+	printf("long double:%3ld bytes\n", sizeof(long double));
+	return 0;
+}
